316a07b6e7cce4a3d335413380f9d56f0f88aee6
gregorykeylam <gregorykeylam@gmail.com>
2025-02-26 01:11:35 -0800

    Added test code for user menu

--- src/OutdoorParkConcert.py
+++ src/OutdoorParkConcert.py
@@ -6,6 +6,25 @@ FrontSeat = 0,1,2,3,4
 MiddleSeat = 5,6,7,8,9,10
 BackSeat = 11,12,13,14,15,16,17,18,19,20
 
+def  PurchaseSeat ():
+    SeatCount = int(input ("Number of seats desired:  "))
+    SeatType = input  ("Type of seat desired:  ")
+    Name = input ("Enter your name:  ")
+    EmailAddr = input ("Enter your email address:  ")
+
+    TicketCost, MaskFee, SubTotal, Tax, Total = CalulateFees(SeatType, SeatCount)
+
+    print ("Name:  " + Name)
+    print ("Email:  " + EmailAddr)
+    print ("Number of seats:  " + str(SeatCount))
+    print ("Seat Type:  " + SeatType)
+    print ("Seats:  #####")
+    print ("Ticket Cost:  " + str(TicketCost))
+    print ("Mask Fee:  " + str(MaskFee))
+    print ("Subtotal:  " + str(SubTotal))
+    print ("Tax:  " + str(Tax))
+    print ("Total:  " + str(Total)) 
+
 def CalulateFees (SeatType, SeatCount):
 
     PriceOfFrontSeat = 80
@@ -14,10 +33,6 @@ def CalulateFees (SeatType, SeatCount):
 
     MaskFee = 5
     TaxRate = 0.0725
-
-    print ()
-    print(type(TaxRate))
-    print ()
 
     match SeatType:
         case "Front":
@@ -34,43 +49,51 @@ def CalulateFees (SeatType, SeatCount):
     Total = Subtotal + Tax
 
     return (TicketCost, TotalMaskFee, Subtotal, Tax, Total)
-    
-# our test matrix has 4 rows and 10 columns
-N_ROW = 4
-N_COL = 10
 
-# available seat
-available_seat = '.'
+def ViewSeating ():
 
-# create some available seating
-seating = []
-for r in range(N_ROW):
-    row = []
-    for c in range(N_COL):
-        row.append(available_seat)
-    seating.append(row)
+    # our test matrix has 4 rows and 10 columns
+    N_ROW = 10
+    N_COL = 26
 
-# print available seating
-for r in range(N_ROW):
-    print(r+1, end="\t")
-    for c in range(N_COL):
-        print(seating[r][c], end=" ")
-    print()
+    # available seat
+    available_seat = '.'
 
-SeatCount = int (input ("Number of seats desired:  "))
-SeatType = input  ("Type of seat desired:  ")
-Name = input ("Enter your name:  ")
-EmailAddr = input ("Enter your email address:  ")
+    # create some available seating
+    seating = []
+    for r in range(N_ROW):
+        row = []
+        for c in range(N_COL):
+            row.append(available_seat)
+        seating.append(row)
 
-TicketCost, MaskFee, SubTotal, Tax, Total = CalulateFees(SeatType, SeatCount)
+    # print available seating
+    for r in range(N_ROW):
+        print(r+1, end="\t")
+        for c in range(N_COL):
+            print(seating[r][c], end=" ")
+        print() 
 
-print ("Name:  " + Name)
-print ("Email:  " + EmailAddr)
-print ("Number of seats:  " + str(SeatCount))
-print ("Seat Type:  " + SeatType)
-print ("Seats:  #####")
-print ("Ticket Cost:  " + str(TicketCost))
-print ("Mask Fee:  " + str(MaskFee))
-print ("Subtotal:  " + str(SubTotal))
-print ("Tax:  " + str(Tax))
-print ("Total:  " + str(Total))
+
+
+
+while True:
+    print ("[p]     Purchase seat")
+    print ("[v]     View seating")
+    print ("[s]     Search seats purchased by customer's name")
+    print ("[d]     Display all purchase made amd total income")
+    print ("[q]     Quit app")
+    Command = input ("Enter a command:  ")
+    match Command:
+        case "p":
+            PurchaseSeat()
+        case "v":
+            ViewSeating()
+        case "s":
+            print ("S has nothing yet")
+        case "d":
+            print ("D has nothing yet")
+        case "q":
+            exit () 
+        case _:
+            print ("Invalid input.  Please try again...")

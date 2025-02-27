0b293367023b1fb6fd9f72e8017444cb8d4a616d
gregorykeylam <gregorykeylam@gmail.com>
2025-02-26 21:28:03 -0800

    Added test code to persist seating change in a file

--- src/OutdoorParkConcert.py
+++ src/OutdoorParkConcert.py
@@ -5,130 +5,125 @@ The matrix is populated with . since all seats are available
 
 import json
 import string
+import os
 
 # relative path
 pathToFile = "./src/OutdoorParkConcert.json"
 
-# try to open a file and throw a error if it is not found
-try:
-    jsonFile = open(pathToFile, 'w')
-except OSError:
-    print("ERROR: Unable to open the file %s" % pathToFile)
 
 
-FrontSeat = 0,1,2,3,4
-MiddleSeat = 5,6,7,8,9,10
-BackSeat = 11,12,13,14,15,16,17,18,19,20
+def  PurchaseSeat():
+    seatCount = int(input ("Number of seats desired:  "))
+    seatType = input  ("Type of seat desired:  ")
+    name = input ("Enter your name:  ")
+    emailAddr = input ("Enter your email address:  ")
 
-def  PurchaseSeat ():
-    SeatCount = int(input ("Number of seats desired:  "))
-    SeatType = input  ("Type of seat desired:  ")
-    Name = input ("Enter your name:  ")
-    EmailAddr = input ("Enter your email address:  ")
+    ticketCost, maskFee, subtotal, tax, total = Calulate_Fees(seatType, seatCount)
 
-    TicketCost, MaskFee, SubTotal, Tax, Total = CalulateFees(SeatType, SeatCount)
-
-    print ("Name:  " + Name)
-    print ("Email:  " + EmailAddr)
-    print ("Number of seats:  " + str(SeatCount))
-    print ("Seat Type:  " + SeatType)
+    print ("Name:  " + name)
+    print ("Email:  " + emailAddr)
+    print ("Number of seats:  " + str(seatCount))
+    print ("Seat Type:  " + seatType)
     print ("Seats:  #####")
-    print ("Ticket Cost:  " + str(TicketCost))
-    print ("Mask Fee:  " + str(MaskFee))
-    print ("Subtotal:  " + str(SubTotal))
-    print ("Tax:  " + str(Tax))
-    print ("Total:  " + str(Total)) 
+    print ("Ticket Cost:  " + str(ticketCost))
+    print ("Mask Fee:  " + str(maskFee))
+    print ("Subtotal:  " + str(subtotal))
+    print ("Tax:  " + str(tax))
+    print ("Total:  " + str(total)) 
 
-def CalulateFees (SeatType, SeatCount):
-
+def Calulate_Fees (seatType, seatCount):
 
     MaskFee = 5
     TaxRate = 0.0725
-    
-    
-    """ 
-    FrontSeatPrice = 80
-    MiddleSeatPrice = 50
-    BackSeatPrice = 25
-    
-    match SeatType:
-        case "Front":
-            SeatPrice = FrontSeatPrice
-        case "Middle":
-            SeatPrice = MiddleSeatPrice
-        case "Back":
-            SeatPrice = BackSeatPrice """
 
-    TicketCost = SeatCount * SeatPrice
-    TotalMaskFee = SeatCount * MaskFee
+    TicketCost = seatCount * SeatPrice
+    TotalMaskFee = seatCount * MaskFee
     Subtotal = TicketCost + TotalMaskFee
     Tax = Subtotal * TaxRate
     Total = Subtotal + Tax
 
     return (TicketCost, TotalMaskFee, Subtotal, Tax, Total)
 
-def ViewSeating (): 
 
+def Open_File (pathToFile, mode):
+
+    # try to open a file and throw a error if it is not found
+    try:
+        jsonFile = open(pathToFile, mode)
+    except OSError:
+        print("ERROR: Unable to open the file %s" % pathToFile)
+    return jsonFile
+
+def ViewSeating (): 
 
     FrontSeatPrice = 80
     MiddleSeatPrice = 50
     BackSeatPrice = 25
 
-
+    MiddleSeatLowerBoundary = 5
+    BackSeatLowerBoundary = 11
 
     ROW = range(20)
     COL = string.ascii_uppercase
     available_seat = '.'
     
-    Seating = {}
-    for r in ROW:
-        Seating[r] = {}
-        for c in COL:
-            Seating[r][c] = {}
-            Seating[r][c]["Availability"] = available_seat
-            Seating[r][c]["Name"] = str(r) + str(c)
-            if r <= 4:
-                Seating[r][c]["Price"] = FrontSeatPrice
-            elif r >= 5 and r <=10:
-                Seating[r][c]["Price"] = MiddleSeatPrice
-            elif r >= 11 and r <=19:
-                Seating[r][c]["Price"] = BackSeatPrice
-                
+    if os.path.exists(pathToFile):
+        jsonFile = Open_File (pathToFile, "r")
+        Seating = json.load(jsonFile)
+    else:
+        Seating = {}
+        print ()
+        for r in ROW:
+            Seating[r] = {}
+            for c in COL:
+                Seating[r][c] = {}
+                Seating[r][c]["Availability"] = available_seat
+                Seating[r][c]["Name"] = str(r) + str(c)
+                """
+                if r < MiddleSeatLowerBoundary:
+                    Seating[r][c]["Price"] = FrontSeatPrice
+                elif r < BackSeatLowerBoundary:
+                    Seating[r][c]["Price"] = MiddleSeatPrice
+                else:
+                    Seating[r][c]["Price"] = BackSeatPrice
+                """   
+                jsonFile = Open_File (pathToFile, "w")
+                json.dump (Seating, jsonFile, indent=6)
+                jsonFile.close()
+
     print (Seating)
 
     # print available seating
+    print ("\n=====================================================================================")
+    print ("                                       Seating")
+    print ("=====================================================================================")
+    print ()
+    
+    print ("\t", end="")
+    for c in COL:
+        print (c, end=" ")
+    print ("\tType\t\tPrice")
+
+    print ("\t" + "-" * 52 + "\t" + "-" * 6 + "\t\t" + "-" * 5)
+
     for r in ROW:
         print(r, end="\t")
         for c in COL:
-            print(Seating[r][c]["Availability"], end=" ")
+            #print ("==" + str(r) + c)
+            print(Seating[str(r)][c]["Availability"], end=" ") 
+
+        if r < MiddleSeatLowerBoundary:
+            SType = "Front"
+            SPrice = FrontSeatPrice
+        elif r < BackSeatLowerBoundary:
+            SType = "Middle"
+            SPrice = MiddleSeatPrice
+        else:
+            SType = "Back"
+            SPrice = BackSeatPrice
+        
+        print("\t" + SType + "\t\t$" + str(SPrice), end="\t")
         print()  
-
-    """ # our test matrix has 4 rows and 10 columns
-    # available seat
-    available_seat = '.'
-
-    # create some available seating
-    seating = []
-    for r in range(N_ROW):
-        row = []
-        for c in range(N_COL):
-            row.append(available_seat)
-        seating.append(row)
-
-    # print available seating
-    for r in range(N_ROW):
-        print(r+1, end="\t")
-        for c in range(N_COL):
-            print(seating[r][c], end=" ")
-        print()  
-"""
-
-
-    json.dump (Seating, jsonFile, indent=6)
-    jsonFile.close()
-
-
-
 
 while True:
     print ("[p]     Purchase seat")
